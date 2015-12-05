65a49b283bcf8465816b0d25d5577b03edd63c23
Bahram Moinvaziri <bramoin@yahoo.com>
2015-12-05 14:03:27 -0800

    Initial commit.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using AHCWebDeveloperExam.Models;
+
+
+namespace AHCWebDeveloperExam.Controllers
+{
+    public class HomeController : Controller
+    {        
+        public ActionResult Index(string DateRange)
+        {
+            if (string.IsNullOrEmpty(DateRange))
+            {
+                DateRange = "WeekOverWeek";
+            }
+            string url = "http://ahcwebapi.azurewebsites.net/api/OrderSummary/" + DateRange;
+            WebRequest request = WebRequest.Create(url);
+            WebResponse ws = request.GetResponse();
+            
+            List<OrderSummary> OrderSummaryList = null;
+
+            using (Stream stream = ws.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                String responseString = reader.ReadToEnd();
+
+               OrderSummaryList = JsonConvert.DeserializeObject<List<OrderSummary>>(responseString);
+            }
+
+            return View(OrderSummaryList);
+        }
+
+    }
+}
---
+++ Models/OrderSummary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AHCWebDeveloperExam.Models
+{
+    public class OrderSummary
+    {
+        public int Id { get; set; }
+        public DateTime PeriodDate { get; set; }
+        public string PeriodLabel { get; set; }
+        public int Ordered { get; set; }
+        public int Shipped { get; set; }
+        public int Remainder { get; set; }
+        public double TotalOrderedAmount { get; set; }
+        public double TotalShippedAmount { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Id: {0}, PeriodDate: {1}, PeriodLabel:{2}, Ordered:{3}, Shipped:{4}, Remainder:{5}, TotalOrderedAmount:{6}, TotalShippedAmount:{7}",
+                Id, PeriodDate, PeriodLabel, Ordered, Shipped, Remainder, TotalOrderedAmount, TotalShippedAmount);
+        }
+    }
+}
